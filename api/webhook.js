876e6db0d84076a9b3a2359bcb8d46015d6e4c17
Naelalkha <nael.alkha@gmail.com>
2025-07-23 15:10:44 +0200

    webhook credentials

--- api/webhook.js
+++ api/webhook.js
@@ -3,17 +3,13 @@ import Stripe from 'stripe';
 import { initializeApp, cert, getApps } from 'firebase-admin/app';
 import { getFirestore } from 'firebase-admin/firestore';
 
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY || 'sk_test_51RnceePEdl4W6QSBMc4OlzTmMDM7ta64GPMF7kSCdsGUnStPGiJo5fM2h8L49KK01A0WuHHw6W5RwznMogVf3SIj00g99xK482');
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
 // Initialiser Firebase Admin
 let app;
 if (!getApps().length) {
   app = initializeApp({
-    credential: cert({
-      projectId: process.env.FIREBASE_PROJECT_ID,
-      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-      privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n')
-    })
+    credential: cert(JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT))
   });
 } else {
   app = getApps()[0];
