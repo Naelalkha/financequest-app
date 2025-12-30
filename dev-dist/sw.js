91cb6ff390b82da58a759cdc8bcf62d4e076ed35
Naelalkha <nael.alkha@gmail.com>
2025-12-30 03:00:30 +0100

    fix tsx

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-f001acab'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.uolt29tdpbg"
+    "revision": "0.pt65dclgjr8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
